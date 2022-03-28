00c6fb880640d9d3c1748fbc451d1a60081c4070
Dane <danejwhitfield@gmail.com>
2022-03-28 16:30:50 +0100

    updated functions in model

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,26 +1,27 @@
 const db = require("../db/connection");
 
 exports.findArticleById = (article_id) => {
-  //   if (article_id > ) {
-  //     return Promise.reject({ status: 404, msg: "not found!" });
-  //   }
-
   return db
     .query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
     .then((result) => {
+      if (result.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "not found!" });
+      }
       return result.rows[0];
     });
 };
 
 exports.changeVotesByArticleId = (article_id, inc_votes) => {
-  if (typeof article_id !== "number") {
-  }
   return db
     .query(
       `UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *;`,
       [inc_votes, article_id]
     )
     .then((result) => {
+      if (result.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "not found!" });
+      }
+
       return result.rows[0];
     });
 };
