7a972100d25bbf2a1a030b19dc529e33be74acd1
Dane <danejwhitfield@gmail.com>
2022-03-28 15:26:38 +0100

    created controller for articles

---
+++ controllers/articles.controller.js
@@ -0,0 +1,23 @@
+const {
+  findArticleById,
+  changeVotesByArticleId,
+} = require("../models/articles.model");
+
+exports.getArticleById = (req, res, next) => {
+  const { article_id } = req.params;
+  findArticleById(article_id)
+    .then((article) => {
+      res.status(200).send({ article: article });
+    })
+    .catch(next);
+};
+
+exports.updateVotesByArticleId = (req, res, next) => {
+  const { article_id } = req.params;
+  const { inc_votes } = req.body;
+  changeVotesByArticleId(article_id, inc_votes)
+    .then((article) => {
+      res.status(201).send({ article: article });
+    })
+    .catch(next);
+};
