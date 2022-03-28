3171c84fe1a596e18b42195decacaf039f36bf2e
Dane <danejwhitfield@gmail.com>
2022-03-28 16:29:34 +0100

    updated error handling in app.js

--- app.js
+++ app.js
@@ -16,12 +16,25 @@ app.use((req, res, next) => {
   res.status(404).send({ msg: "not found!" });
 });
 
+// PG ERROR HANDLERS
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "bad request!" });
+  }
+  next(err);
+});
+
 // CUSTOM HANDLER
 app.use((err, req, res, next) => {
   if (err.msg && err.status) {
     res.status(err.status).send({ msg: err.msg });
   }
+  next(err);
 });
+
+// app.use((err, req, res, next) => {
+//   res.status(400).send({ msg: "bad request!" });
+// });
 
 // DEFAULT
 app.use((err, req, res, next) => {
