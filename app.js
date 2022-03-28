5f9ce68f862cdc68734f9275abc4ba50c4debe1b
Dane <danejwhitfield@gmail.com>
2022-03-28 18:16:51 +0100

    updated app.js with new endpoints

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const { getArticleById } = require("./controllers/articles.controller");
 const { getTopics } = require("./controllers/topics.controller");
+const { getUsers } = require("./controllers/users.controller");
 const app = express();
 
 app.use(express.json());
 
+// GET
 app.get("/api/topics", getTopics);
 app.get("/api/articles/:article_id", getArticleById);
+app.get("/api/users", getUsers);
 
 // GENERIC
 app.use((req, res, next) => {
