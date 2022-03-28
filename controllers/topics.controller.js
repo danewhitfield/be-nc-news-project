b35ea39b00e004279ad7f65abb3cab95e29a0d83
Dane <danejwhitfield@gmail.com>
2022-03-28 11:59:40 +0100

    updated controller

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -2,8 +2,7 @@ const { findTopics } = require("../models/topics.model");
 
 exports.getTopics = (req, res, next) => {
   findTopics()
-    .then((result) => {
-      const topics = result.rows;
+    .then((topics) => {
       res.status(200).send(topics);
     })
     .catch(next);
