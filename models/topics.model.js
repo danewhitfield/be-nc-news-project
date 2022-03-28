abc8a6a4e84ff8e703fdaf3d55ce21980dc142f1
Dane <danejwhitfield@gmail.com>
2022-03-28 11:59:59 +0100

    updated model

--- models/topics.model.js
+++ models/topics.model.js
@@ -1,5 +1,7 @@
 const db = require("../db/connection");
 
 exports.findTopics = () => {
-  return db.query(`SELECT * FROM topics;`);
+  return db.query(`SELECT * FROM topics;`).then((result) => {
+    return result.rows;
+  });
 };
