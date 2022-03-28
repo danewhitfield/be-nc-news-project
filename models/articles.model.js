4b2f1ebff9cb2e448757359df6edd54a7ec7d28f
Dane <danejwhitfield@gmail.com>
2022-03-28 15:30:47 +0100

    fixed 404 error handling

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,15 +1,12 @@
 const db = require("../db/connection");
-const articles = require("../db/data/test-data/articles");
 
 exports.findArticleById = (article_id) => {
-  console.log("article_id:", article_id);
-  if (article_id > articles.length) {
-    return Promise.reject({ status: 404, msg: "not found!" });
-  }
-
   return db
     .query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
     .then((result) => {
+      if (result.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "not found!" });
+      }
       return result.rows[0];
     });
 };
