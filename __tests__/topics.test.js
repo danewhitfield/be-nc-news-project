d1f2a23dc2421f4d1464037fc46120d9818d6a9b
Dane <danejwhitfield@gmail.com>
2022-03-28 11:41:03 +0100

    created test file for topics

---
+++ __tests__/topics.test.js
@@ -0,0 +1,34 @@
+const db = require("../db/connection");
+const request = require("supertest");
+const testData = require("../db/data/test-data");
+const app = require("../app");
+const seed = require("../db/seeds/seed");
+
+afterAll(() => db.end());
+beforeEach(() => seed(testData));
+
+describe("GET /api/topics", () => {
+  it("returns an array of objects with description and slug", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then((res) => {
+        const topics = [
+          {
+            description: "The man, the Mitch, the legend",
+            slug: "mitch",
+          },
+          {
+            description: "Not dogs",
+            slug: "cats",
+          },
+          {
+            description: "what books are made of",
+            slug: "paper",
+          },
+        ];
+
+        expect(res.body).toEqual(topics);
+      });
+  });
+});
