21c4afaa286778493d6e183e1c49576e4ccfaf47
Dane <danejwhitfield@gmail.com>
2022-03-29 14:36:14 +0100

    added func to model for article to find comments

--- models/articles.model.js
+++ models/articles.model.js
@@ -32,3 +32,14 @@ exports.changeVotesByArticleId = (article_id, inc_votes) => {
       return result.rows[0];
     });
 };
+
+exports.findComments = (article_id) => {
+  return db
+    .query(`SELECT * FROM comments WHERE article_id = $1`, [article_id])
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "not found!" });
+      }
+      return result.rows;
+    });
+};
